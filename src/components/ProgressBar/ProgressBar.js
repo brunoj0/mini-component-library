c9549c5499b383d964d110b9b442bb3c19de95f1
brunoj <bruno.jamula@gmail.com>
2023-10-19 15:33:42 +0200

    implement ProgressBar and Select

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,77 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+const SIZES = {
+  small: {
+    "--borderRadius": 4 + "px",
+    "--height": 8 + "px",
+    "--padding": "0",
+  },
+  medium: {
+    "--borderRadius": 4 + "px",
+    "--height": 12 + "px",
+    "--padding": "0",
+  },
+  large: {
+    "--borderRadius": 8 + "px",
+    "--height": 16 + "px",
+    "--padding": "4px",
+  },
 };
 
+const ProgressBar = ({ value, size }) => {
+  if (value > 100) {
+    value = 100;
+  }
+  const styles = SIZES[size];
+  const ProgressBarContainer = styled.div`
+    display: flex;
+    align-items: center;
+    width: 487px;
+    justify-content: space-between;
+    font-weight: bold;
+    overflow: hidden;
+  `;
+
+  const ProgressContainer = styled.div`
+    width: 370px;
+    height: var(--height);
+    box-shadow: inset 0px 2px 4px 0 ${COLORS.transparentGray35};
+    background-color: ${COLORS.transparentGray15};
+    padding: var(--padding);
+    border-radius: var(--borderRadius);
+    overflow: hidden;
+  `;
+
+  const ProgressBarValue = styled.div`
+    width: ${value + `%`};
+    height: 100%;
+    background-color: ${COLORS.primary};
+    border-radius: 4px 0 0 4px;
+  `;
+  const BarContainer = styled.div`
+    border-radius: 4px;
+    height: 100%;
+    overflow: hidden;
+  `;
+  return (
+    <ProgressBarContainer
+      role="progressbar"
+      aria-valuemin="0"
+      aria-valuemax="100"
+      aria-valuenow={value}
+    >
+      <span>{value}%</span>
+      <ProgressContainer style={styles}>
+        <BarContainer>
+          <ProgressBarValue value={value}></ProgressBarValue>
+        </BarContainer>
+      </ProgressContainer>
+    </ProgressBarContainer>
+  );
+};
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <OriginalSelect value={value} onChange={onChange}>
+        {children}
+      </OriginalSelect>
+      <CustomSelect>
+        <SelectValue>{displayedValue}</SelectValue>
+        <Icon id="chevron-down"></Icon>
+      </CustomSelect>
+    </Wrapper>
   );
 };
+const Wrapper = styled.div`
+  position: relative;
+  display: flex;
+  height: 43px;
+  width: max-content;
+`;
+const SelectValue = styled.span`
+  display: flex;
+  padding-right: 16px;
+`;
 
+const OriginalSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+`;
+const CustomSelect = styled.div`
+  display: flex;
+  align-items: center;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 4px;
+  padding: 12px 16px;
+  ${OriginalSelect}:focus + & {
+    outline: 1px dotted #212121;
+    outline: 5px auto ${COLORS.primary};
+  }
+
+  ${OriginalSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 export default Select;
